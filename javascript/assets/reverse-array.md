4efca0da122e2d45fd8bf094060e840df1e20e01
jonathon lee <jonnyleealas@gmail.com>
2020-08-17 16:37:32 -0700

    challenge3

---
+++ javascript/assets/reverse-array.md
@@ -0,0 +1,2 @@
+[reverse an array](javascript/challenges/arrayReverse/arra-reverse.js)
+![Untitled_Artwork](https://user-images.githubusercontent.com/63610026/90168256-afd0af00-dd51-11ea-8c8a-a2d2cfac07a0.jpg)
---
+++ javascript/challenges/array-binary-search/array-binary-search.js
@@ -0,0 +1,28 @@
+const keyIndex = 23;
+const array = [4,8,15,16,23,42]
+const arr2 = Array.from(Array(1000000).keys())
+
+function binarySearch (arr, key){
+  let start = 0;
+  let end = arr.length -1;
+
+  while ( start <= end){
+    // want it to round down because rounding ceil will miss numbers
+    let middle = Math.floor((start + end)/2);
+    //check best case senmerio by checking to see if middle matches the key number. return middle
+    if( arr[middle] === key){
+
+      return middle
+    }
+    if(arr[middle] < key){
+      start = middle + 1
+
+    } else {
+      end = middle -1
+
+    }
+  }
+  return -1
+}
+
+console.log(binarySearch(array, keyIndex), array[binarySearch(array, keyIndex)], keyIndex);
